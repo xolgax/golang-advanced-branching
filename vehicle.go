3c3f4dbf77f19cab127a0b67d792db6c7f98ddd7
xolgax <54672640+xolgax@users.noreply.github.com>
2022-09-05 16:39:48 -0500

    Update vehicle.go
    fixed format even though there is nothing wrong with the one I used.

--- vehicle.go
+++ vehicle.go
@@ -148,7 +148,7 @@ func generateRating() {
 }
 
 func showRating(model string) {
-	var ratingFound bool = false
+	ratingFound := false
 	for m, r := range vehicleResult {
 		if m == model {
 			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
