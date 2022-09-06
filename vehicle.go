e34d47e3887d4ce716d09d515e205b2d03be2991
xolgax <54672640+xolgax@users.noreply.github.com>
2022-09-05 21:04:14 -0500

    Update vehicle.go
    update to try and get step 11 to work

--- vehicle.go
+++ vehicle.go
@@ -83,13 +83,13 @@ func main() {
 	// Print ratings for the different vehicles
 
 	for _, veh := range inventory {
-		switch v := veh.(type); v {
+		switch v := veh; v {
 		case car:
-			v.carDetails()
+			carDetails()
 		case bike:
-			v.bikeDetails()
+			bikeDetails()
 		case truck:
-			v.truckDetails()
+			truckDetails()
 		default:
 			fmt.Printf("Are you sure this Vehicle Type exists")
 		}
@@ -161,7 +161,8 @@ func showRating(model string) {
 	}
 }
 
-func carDetails(c *car) {
+func carDetails() {
+	c := *car
 	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
 	showRating(c.model)
 }
